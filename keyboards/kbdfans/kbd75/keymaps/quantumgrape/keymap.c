9205653843b67a735af53faaa898f27db653c470
Karl Berggren <berggren.karl@gmail.com>
2020-08-09 23:01:24 +0200

    updated keymap

--- keyboards/kbdfans/kbd75/keymaps/quantumgrape/keymap.c
+++ keyboards/kbdfans/kbd75/keymaps/quantumgrape/keymap.c
@@ -16,7 +16,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case KEY_LULU:
         if (record->event.pressed) {
             // when keycode QMKBEST is pressed
-            SEND_STRING("ʕっ•ᴥ•ʔっ ");
+            SEND_STRING("<3");
         } else {
             // when keycode QMKBEST is released
         }
@@ -24,7 +24,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case KEY_ROCIO:
         if (record->event.pressed) {
             // when keycode QMKBEST is pressed
-            SEND_STRING("(✿◠‿◠)");
+            SEND_STRING(":P");
         } else {
             // when keycode QMKBEST is released
 
@@ -33,10 +33,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case KEY_KARL:
         if (record->event.pressed) {
             // when keycode QMKBEST is pressed
-            SEND_STRING("♪♬");
+            SEND_STRING("exp(pi*i)+1=");
         } else {
             // when keycode QMKBEST is released
-            SEND_STRING(" ᕕ(⌐■_■)ᕗ ♪♬");
+            SEND_STRING("0");
         }
         break;
     }
@@ -46,7 +46,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_iso_1u(
         KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,           KC_F12,   KC_PSCR,  MO(2),      KC_DEL,
-        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,          KC_EQL,   KC_DEL,   KC_BSPC,    KC_HOME,
+        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,          KC_EQL,   KC_BSPC,              KC_HOME,
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,          KC_RBRC,  KC_ENT,               KC_PGUP,
         KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,          KC_NUHS,                        KC_PGDN,
         KC_LSFT,  KC_NUBS,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,          KC_RSFT,            KC_UP,      KC_END,
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [1] = LAYOUT_iso_1u(
         _______,  KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,  KC_BRID,  KC_BRIU,  _______,  _______,  _______,  _______,  _______,          _______,  KC_PAUS,  KC_SLCK,    KC_INS,
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,          _______,  _______,  _______,    _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,          _______,  _______,              _______,
         _______,  _______,  _______,  _______,  KEY_ROCIO, _______,  _______,  _______,  _______, _______,  _______,  _______,          _______,  _______,              _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KEY_KARL, KEY_LULU,  _______,  _______,         _______,                        _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,          _______,            _______,    _______,
@@ -64,9 +64,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [2] = LAYOUT_iso_1u(
         _______,  DM_REC1,  DM_REC2,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_TRNS,  _______,
-        _______,  DM_PLY1,  DM_PLY2,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,
+        _______,  DM_PLY1,  DM_PLY2,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
         _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,            _______,
-        _______,  _______,  DM_RSTP,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,                      _______,
+        _______,  _______,  DM_RSTP,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,                        _______,
         _______,  _______,  _______,  _______,  _______,  _______,  RESET,    _______,  _______,  _______,  _______,  _______,  _______,            RGB_VAI,  RGB_TOG,
         _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  RGB_RMOD, RGB_VAD,  RGB_MOD
     ),
